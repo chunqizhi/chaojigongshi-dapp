93fb71df1cedea84f20eeff88d24daa7a576d357
chunqizhi <1558763837@qq.com>
2020-08-24 10:02:44 +0000

    Add project

---
+++ mine.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp -r /workspace/dapp/miner/data/keystore/* ~/data/keystore/
+geth -datadir ~/data/ --networkid 4857 --allow-insecure-unlock --ws --wsaddr 0.0.0.0 --wsorigins="*" --rpc --rpcaddr "172.20.0.50" --rpcapi admin,eth,miner,web3,personal,net,txpool --unlock "0x827567308e08628d2a769EDA8649fF2853276e3a" --etherbase "0x827567308e08628d2a769EDA8649fF2853276e3a" console
